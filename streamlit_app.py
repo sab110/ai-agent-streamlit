510ad9e28911ac2f41993cc5813836bc9f2f607a
sab110 <140268593+sab110@users.noreply.github.com>
2024-11-15 16:49:23 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,6 @@ from dotenv import load_dotenv
 import os
 import re
 
-import pysqlite3
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 
 # Load environment variables
 load_dotenv()
